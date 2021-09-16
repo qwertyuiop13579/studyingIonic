1b2d9aa25e9e95d6462b58b95a6933ed56ce4e30
Nikita Kharlap <harlap19@gmail.com>
2021-09-16 18:09:32 +0300

    change discover page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../place.model';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-discover',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoverPage implements OnInit {
 
-  constructor() { }
+  places: Place[] = [];
+
+  constructor(private placesService: PlacesService) { }
 
   ngOnInit() {
+    this.places = this.placesService.places;
+
   }
+
+
 
 }
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+/* eslint-disable no-underscore-dangle */
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place('pl1', 'title1', 'description1', 'https://www.pandotrip.com/wp-content/uploads/2018/06/Eiffel-Tower-Paris-France.jpg', 10),
+    new Place('pl2', 'title2', 'description2', 'https://media.gettyimages.com/photos/the-statue-of-liberty-with-world-trade-center-background-landmarks-of-picture-id1059614218?s=612x612', 15),
+    new Place('pl3', 'title3', 'description3', 'https://image.shutterstock.com/image-photo/street-white-houses-colonia-shown-260nw-1035199813.jpg', 20),
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+
+  constructor() { }
+}
