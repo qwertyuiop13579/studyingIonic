1ff0d5daee9fb544a48cc06cf1ebce070eb20084
Nikita Kharlap <harlap19@gmail.com>
2021-09-17 12:39:45 +0300

    add side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
+  onLogout() {
+    console.log('Logout');
+  }
 }
