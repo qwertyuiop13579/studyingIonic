6a9dd7d0d1978990e8d3533a061b830a140e87b6
Nikita Kharlap <harlap19@gmail.com>
2021-09-24 10:15:27 +0300

    add pickedLocation preview

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-map-modal',
@@ -47,7 +48,7 @@ export class MapModalComponent implements OnInit, AfterViewInit {
     }
     return new Promise((resolve, reject) => {
       const script = document.createElement('script');
-      script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY';
+      script.src = 'https://maps.googleapis.com/maps/api/js?key=' + environment.googleMapsAPIKey;
       script.async = true;
       script.defer = true;
       document.body.appendChild(script);
--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,6 +1,12 @@
+/* eslint-disable max-len */
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
+import { environment } from '../../../../environments/environment';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from '../../../places/location.model';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
@@ -9,7 +15,10 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
 })
 export class LocationPickerComponent implements OnInit {
 
-  constructor(private modalCtrl: ModalController) { }
+  selectedLocationImage: string;
+  isLoading = false;
+
+  constructor(private modalCtrl: ModalController, private http: HttpClient) { }
 
   ngOnInit() { }
 
@@ -19,10 +28,41 @@ export class LocationPickerComponent implements OnInit {
     })
       .then(modalEl => {
         modalEl.onDidDismiss().then(modalData => {
-          console.log(modalData.data);
+          if (!modalData.data) {
+            return;
+          }
+          const pickedLocation: PlaceLocation = {
+            lat: modalData.data.lat,
+            lng: modalData.data.lng,
+            address: null,
+            staticMapImageURL: null,
+          };
+          this.isLoading = true;
+          this.getAddress(modalData.data.lat, modalData.data.lng).pipe(switchMap((address) => {
+            pickedLocation.address = address;
+            return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+          })).subscribe((staticMapImageURL) => {
+            pickedLocation.staticMapImageURL = staticMapImageURL;
+            this.selectedLocationImage = staticMapImageURL;
+            this.isLoading = false;
+          });
         });
         modalEl.present();
       })
       .then(resultData => { });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.http.get<any>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapsAPIKey}`)
+      .pipe(map((geoData: any) => {
+        if (!geoData || !geoData.results || geoData.results.length === 0) {
+          return null;
+        }
+        return geoData.results[0].formatted_address;
+      }));
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&markers=color:red%7Clabel:Place%7C${lat},${lng}&key=${environment.googleMapsAPIKey}`;
   }
 }
