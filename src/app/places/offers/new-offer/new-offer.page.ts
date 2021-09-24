fd52356d26a6d5e5b30f0d99fdb62feac424cdcd
Nikita Kharlap <harlap19@gmail.com>
2021-09-24 17:13:25 +0300

    add picked image preview

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
 /* eslint-disable object-shorthand */
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -6,6 +7,30 @@ import { LoadingController } from '@ionic/angular';
 
 import { PlaceLocation } from '../../location.model';
 import { PlacesService } from '../../places.service';
+
+function base64toBlob(base64Data, contentType) {     //convert Base64 to blob
+  contentType = contentType || '';
+  const sliceSize = 1024;
+  const byteCharacters = atob(base64Data);
+  const bytesLength = byteCharacters.length;
+  const slicesCount = Math.ceil(bytesLength / sliceSize);
+  const byteArrays = new Array(slicesCount);
+
+  for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+    const begin = sliceIndex * sliceSize;
+    const end = Math.min(begin + sliceSize, bytesLength);
+
+    const bytes = new Array(end - begin);
+    for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+      bytes[i] = byteCharacters[offset].charCodeAt(0);
+    }
+    byteArrays[sliceIndex] = new Uint8Array(bytes);
+  }
+  return new Blob(byteArrays, { type: contentType });
+}
+
+
+
 
 @Component({
   selector: 'app-new-offer',
@@ -25,7 +50,8 @@ export class NewOfferPage implements OnInit {
       price: new FormControl(null, { updateOn: 'blur', validators: [Validators.required, Validators.min(1)] }),
       dateFrom: new FormControl(null, { updateOn: 'blur', validators: [Validators.required] }),
       dateTo: new FormControl(null, { updateOn: 'blur', validators: [Validators.required] }),
-      location: new FormControl(null, { validators: [] })
+      location: new FormControl(null, { validators: [] }),
+      image: new FormControl(null, { validators: [] }),
     });
   }
 
@@ -33,14 +59,27 @@ export class NewOfferPage implements OnInit {
     this.form.patchValue({ location: location });
   }
 
-  onImagePicked(imageData: string){
-    console.log(imageData);
+  onImagePicked(imageData: string | File) {
+    let imageFile;
+    if (typeof imageData === 'string') {
+      try {
+        imageFile = base64toBlob(imageData.replace('data:image/jpeg;base64,', ''), 'image/jpeg');
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+    }
+    else {
+      imageFile = imageData;
+    }
+    this.form.patchValue({ image: imageFile });
   }
 
   onCreateOffer() {
-    if (this.form.invalid) {
+    if (this.form.invalid || !this.form.get('image').value) {
       return;
     }
+    console.log(this.form.value);
     this.loadingCtrl.create({
       message: 'Creating new place...'
     }).then(loadingEl => {
--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -8,19 +9,31 @@ import { Capacitor } from '@capacitor/core';
   styleUrls: ['./image-picker.component.scss'],
 })
 export class ImagePickerComponent implements OnInit {
-  @Output() imagePick = new EventEmitter<string>();
+  @ViewChild('filepicker') filePickerRef: ElementRef<HTMLInputElement>;
+  @Output() imagePick = new EventEmitter<string | File>();
   selectedImage: string;
+  canUsePicker = false;
 
-  constructor() { }
+  constructor(private platform: Platform) { }
 
+  ngOnInit() {
+    console.log('Mobile: ' + this.platform.is('mobile'));
+    console.log('Hybrid: ' + this.platform.is('hybrid'));
+    console.log('Ios: ' + this.platform.is('ios'));
+    console.log('Android: ' + this.platform.is('android'));
+    console.log('Desktop: ' + this.platform.is('desktop'));
+    if ((this.platform.is('mobile') && !this.platform.is('hybrid')) || (this.platform.is('desktop'))) {
+      this.canUsePicker = true;
+    }
+  }
 
-  ngOnInit() { }
-
-  onPickImage() {
-    if (!Capacitor.isPluginAvailable('Camera')) {
+  async onPickImage() {
+    if (!Capacitor.isPluginAvailable('Camera') || this.canUsePicker) {
       console.log('Camera is not available.');
+      this.filePickerRef.nativeElement.click();
       return;
     }
+    console.log('Camera is available.');
     Camera.getPhoto({
       quality: 50,
       source: CameraSource.Prompt,
@@ -29,16 +42,26 @@ export class ImagePickerComponent implements OnInit {
       width: 200,
       resultType: CameraResultType.DataUrl
     }).then(image => {
+      console.log('Image is available.');
       this.selectedImage = image.dataUrl;
       this.imagePick.emit(image.dataUrl);
       console.log(image);
     }).catch((error) => {
       console.log(error);
     });
-
-
-
-
   }
 
+  onFileChosen(event: Event) {
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+    if (!pickedFile) {
+      return;
+    }
+    const fr = new FileReader();
+    fr.onload = () => {
+      const dataURL = fr.result.toString();
+      this.selectedImage = dataURL;
+      this.imagePick.emit(pickedFile);
+    };
+    fr.readAsDataURL(pickedFile);
+  }
 }
